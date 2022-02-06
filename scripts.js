72c73081bdb8bb410ee743601c590a0e34be2c74
r4ulzito <raulsouza180802@gmail.com>
2022-02-06 14:28:12 -0300

    StopWatch_JS

---
+++ scripts.js
@@ -0,0 +1,46 @@
+var sec = 0;
+var min = 0;
+var hr = 0;
+var interval;
+var watchText = document.querySelector(".watch");
+
+function start() {
+  watch();
+  interval = setInterval(watch, 1000);
+}
+
+function twoXbutton() {
+  interval = setInterval(watch, 100);
+}
+
+function pause() {
+  clearInterval(interval);
+}
+
+function _stop() {
+  watchText.innerText = "00:00:00";
+  clearInterval(interval);
+}
+
+function twoDigits(digit) {
+  if (digit < 10) {
+    return "0" + digit;
+  } else {
+    return digit;
+  }
+}
+
+function watch() {
+  sec = sec + 1;
+
+  if (sec === 60) {
+    min++;
+    sec = 0;
+    if (min === 60) {
+      min = 0;
+      hr++;
+    }
+  }
+  watchText.innerText =
+    twoDigits(hr) + ":" + twoDigits(min) + ":" + twoDigits(sec);
+}
